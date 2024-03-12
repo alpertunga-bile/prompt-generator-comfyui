b9f3c687583c03c02ad3d4b940657c7d05bc7319
alpertunga-bile <76731692+alpertunga-bile@users.noreply.github.com>
2024-03-12 14:11:16 +0000

    preprocess function is updated with regex

--- generator/preprocess.py
+++ generator/preprocess.py
@@ -1,50 +1,84 @@
-from string import punctuation
-from re import sub, compile
+from re import sub, findall, compile
 from collections import OrderedDict
 
 
 def get_unique_list(sequence: list) -> list:
     seen = set()
-    return [x for x in sequence if not (x in seen or seen.add(x))]
+    return [x for x in sequence if not (x.strip() in seen or seen.add(x.strip()))]
 
 
 def remove_exact_keywords(line: str) -> list[str]:
-    char_blacklist = set(f"{punctuation}0123456789")
-
     # remove exact prompts
     prompts = get_unique_list(line.split(","))
+
+    remove_nonprompts_regex = compile(r"[^a-zA-Z()\[\]{}]*")
+    remove_nonweighters_regex = compile(r"[()\[\]{}]*")
+    remove_inside_regex = compile(r"[^()\[\]{}]*")
+    empty_parantheses_remove_regex = compile(r"\(\s*\)")
 
     pure_prompts = OrderedDict()  # order matters
     extracted_pure_prompts = {}  # order isn't important
 
     # remove exact keyword
     for prompt in prompts:
-        # extract the keyword
-        keyword = "".join(c for c in prompt if c not in char_blacklist).lstrip()
+        tempPrompt = sub(remove_nonprompts_regex, "", prompt).lstrip()
 
-        if keyword == "":
+        if tempPrompt == "":
             continue
 
-        if prompt in pure_prompts or keyword in extracted_pure_prompts:
+        tempPrompt = sub(remove_nonweighters_regex, "", tempPrompt)
+
+        if tempPrompt in extracted_pure_prompts:
+            tempPrompt = sub(remove_inside_regex, "", prompt)
+
+            if (
+                len(findall(empty_parantheses_remove_regex, tempPrompt)) > 0
+                or tempPrompt == ""
+            ):
+                continue
+
+            pure_prompts[tempPrompt] = True
             continue
 
-        pure_prompts[prompt] = True
-        extracted_pure_prompts[keyword] = True
+        if tempPrompt == "":
+            pure_prompts[tempPrompt] = True
+        else:
+            extracted_pure_prompts[tempPrompt] = True
+            pure_prompts[prompt] = True
 
     return pure_prompts.keys()
 
 
 def preprocess(line: str, preprocess_mode: str) -> str:
-    pattern = compile(r"(,\s){2,}")
 
-    temp_line = line.replace("\xa0", " ")
+    temp_line = line.encode("ascii", "xmlcharrefreplace").decode()
+    temp_line = temp_line.encode(errors="xmlcharrefreplace").decode()
+
+    temp_line = temp_line.replace("\xa0", " ")
     temp_line = temp_line.replace("\n", ", ")
     temp_line = temp_line.replace("\t", " ")
-    temp_line = temp_line.replace("  ", " ")
-    temp_line = sub(pattern, ", ", temp_line)
+    temp_line = sub(r"\s+", " ", temp_line)
+    temp_line = sub(r"(,\s){2,}", ", ", temp_line)  # remove non prompt commas
+    temp_line = sub(
+        r",\s*:[0-9]*\.?[0-9]+", "", temp_line
+    )  # remove non prompt scalar weights
 
     if preprocess_mode == "exact_keyword":
-        temp_line = ",".join(remove_exact_keywords(temp_line))
+        temp_line = ", ".join(remove_exact_keywords(temp_line))
+
+        temp_line = temp_line.replace("(,", "(")
+        temp_line = temp_line.replace("[,", "[")
+        temp_line = temp_line.replace("{,", "{")
+
+        temp_line = sub(
+            r"\s+", " ", temp_line
+        )  # replace multiple whitespaces with one whitespace
+        temp_line = sub(r"\s+,", "", temp_line)  # remove nonprompt commas
+
+        temp_line = temp_line.replace(", )", ")")
+        temp_line = temp_line.replace(", ]", "]")
+        temp_line = temp_line.replace(", }", "}")
+
     elif preprocess_mode == "exact_prompt":
         temp_line = ",".join(get_unique_list(temp_line.split(",")))
 
