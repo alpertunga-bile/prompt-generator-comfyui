553ec92962c9311e9c1c6327cbbca6c5de1939ba
alpertunga-bile <holy_celestine@hotmail.com>
2025-01-12 15:31:44 +0300

    ComfyUIManager installation scripts are added

---
+++ install.py
@@ -0,0 +1,44 @@
+import subprocess
+import sys
+import platform
+import generator.utility
+
+common_packages = [
+    "transformers",
+    "accelerate",
+    "optimum",
+    "optimum[onnxruntime-gpu]",
+    "transformers[sentecepiece]",
+    "peft",
+]
+
+
+def install_packages(packages: list[str]) -> None:
+    for package in packages:
+        print(f"/_\\ Installing {package}")
+        command = f"{sys.executable} -m pip install {package}"
+        process = subprocess.run(command, shell=True, check=True, capture_output=True)
+
+        if process.returncode != 0:
+            print(f"{package} installation is failed\nError: {process.stdout}")
+
+
+if __name__ == "__main__":
+    print(" Prompt Generator ComfyUI Node ".center(100, "-"))
+    print("/_\\ Thank you for installing Prompt Generator node")
+
+    packages_to_install = []
+    packages_to_install.extend(common_packages)
+
+    os_name = platform.system()
+
+    if os_name == "Linux":
+        packages_to_install.append("triton")
+        packages_to_install.append("bitsandbytes")
+
+    if os_name != "Linux" and generator.utility.check_torch_version_is_enough(2, 4):
+        packages_to_install.append("optimum-quanto")
+
+    install_packages()
+
+    print("/_\\ Installation is compeleted successfully")
